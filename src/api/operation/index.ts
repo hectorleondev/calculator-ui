7e3cf9e4fdb3e1376c8aa6a42ce806417633aa84
hector ramon leon <payorayo@hectors-MacBook-Pro.local>
2023-04-27 06:57:27 -0700

    Fix: fixed bug

--- src/api/operation/index.ts
+++ src/api/operation/index.ts
@@ -3,7 +3,7 @@ import {OperationList} from "./types";
 
 class OperationService {
     get_operation_list(token: string) {
-        return http.get<OperationList>("/dev/operation/list", {headers: { Authorization: 'Bearer ' + token }})
+        return http.get<OperationList>("/operation/list", {headers: { Authorization: 'Bearer ' + token }})
     }
 }
 export default new OperationService();
